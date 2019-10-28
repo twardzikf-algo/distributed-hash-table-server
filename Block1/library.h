d17846b29185398e02136380ee764ff4fafa2d42
lfx2m <lfx2m@o2.pl>
2019-10-28 14:10:45 +0100

    block 1 add

---
+++ Block1/library.h
@@ -0,0 +1,6 @@
+#ifndef BLOCK1_LIBRARY_H
+#define BLOCK1_LIBRARY_H
+
+void hello(void);
+
+#endif //BLOCK1_LIBRARY_H
---
+++ Block1/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("hello nr 2\n");
+    return 0;
+}
