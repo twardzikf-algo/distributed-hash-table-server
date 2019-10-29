5cd514697ea10a4e069a99b7590a83608b7f55c4
lfx2m <lfx2m@o2.pl>
2019-10-29 14:47:02 +0100

    Removed Block1, redoing it

--- Block1/library.h
+++
@@ -1,6 +0,0 @@
-#ifndef BLOCK1_LIBRARY_H
-#define BLOCK1_LIBRARY_H
-
-void hello(void);
-
-#endif //BLOCK1_LIBRARY_H
--- Block1/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    printf("hello nr 2\n");
-    return 0;
-}
